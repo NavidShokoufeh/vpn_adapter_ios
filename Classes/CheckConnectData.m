74ccf659001009a3adb4f68fbc77821d1f63a79a
Navid Shokoufeh <navid.shekofeh@gmail.com>
2025-01-31 16:34:07 +0330

    initial commit

---
+++ Classes/CheckConnectData.m
@@ -0,0 +1,98 @@
+//
+//  CheckConnectData.m
+//  openconnect-example
+//
+//  Created by NavidShokoufeh on 1403-08-01.
+//
+
+#import "CheckConnectData.h"
+#import "NodeModel.h"
+
+@implementation CheckConnectData
+
++(NSDictionary*)checkConnectParmer:(NodeModel*)modal {
+    if([modal.nodeName isEqualToString:@"SSH"]) {
+        return @{@"type":@"sshtunnel",
+                 @"username":modal.username,
+                 @"password":modal.password,
+                 @"address":modal.server,
+                 @"port":modal.port,
+                 @"allowInsecure":@(modal.TLS),
+                 @"chap_enable":@(modal.CHAP),
+                 @"pap_enable":@(modal.PAP),
+                 @"mschap2_enable":@(modal.MSCHAP2),
+                 @"udpgw":modal.udpgw,
+                 @"udpgw_port":modal.udpgw_port,};
+    }else if([modal.nodeName isEqualToString:@"SSTP"]){
+        return @{@"type":@"sstp",
+                 @"username":modal.username,
+                 @"password":modal.password,
+                 @"address":modal.server,
+                 @"port":modal.port,
+                 @"chap_enable":@(modal.CHAP),
+                 @"mschap2_enable":@(modal.MSCHAP2),
+                 @"pap_enable":@(modal.PAP),
+                 @"allowInsecure":@(modal.TLS),
+                };
+    }else if([modal.nodeName isEqualToString:@"openConnect"]){
+        return @{@"type":@"openconnect",
+                 @"username":modal.username,
+                 @"password":modal.password,
+                 @"address":modal.server,
+                 @"port":modal.port,
+                 @"chap_enable":@(modal.CHAP),
+                 @"mschap2_enable":@(modal.MSCHAP2),
+                 @"pap_enable":@(modal.PAP),
+                 @"allowInsecure":@(modal.TLS),
+                };
+    }
+    return @{};
+}
+
++(NodeModel*)getModalForDic:(NSDictionary*)selectDic {
+    if([selectDic[@"type"] isEqualToString:@"sshtunnel"]) {
+        NodeModel * modal = [NodeModel new];
+        modal.nodeName = @"SSH";
+        modal.username =selectDic[@"username"];
+        modal.password =selectDic[@"password"];
+        modal.server =selectDic[@"address"];
+        modal.port =selectDic[@"port"];
+        modal.TLS =[selectDic[@"allowInsecure"] boolValue];
+        modal.PAP =[selectDic[@"pap_enable"] boolValue];
+        modal.CHAP =[selectDic[@"chap_enable"] boolValue];
+        modal.MSCHAP2 =[selectDic[@"mschap2_enable"] boolValue];
+        modal.udpgw =selectDic[@"udpgw"];
+        modal.udpgw_port =selectDic[@"udpgw_port"];
+        return modal;
+        
+    }else if([selectDic[@"type"] isEqualToString:@"sstp"]) {
+        NodeModel * modal = [NodeModel new];
+        modal.nodeName = @"SSTP";
+        modal.username =selectDic[@"username"];
+        modal.password =selectDic[@"password"];
+        modal.server =selectDic[@"address"];
+        modal.PAP =[selectDic[@"pap_enable"] boolValue];
+        modal.CHAP =[selectDic[@"chap_enable"] boolValue];
+        modal.MSCHAP2 =[selectDic[@"mschap2_enable"] boolValue];
+        modal.port =selectDic[@"port"];
+        modal.TLS =[selectDic[@"allowInsecure"] boolValue];
+        return modal;
+
+    }else if([selectDic[@"type"] isEqualToString:@"openconnect"]){
+        NodeModel * modal = [NodeModel new];
+        modal.nodeName = @"openConnect";
+        modal.username =selectDic[@"username"];
+        modal.password =selectDic[@"password"];
+        modal.server =selectDic[@"address"];
+        modal.PAP =[selectDic[@"pap_enable"] boolValue];
+        modal.CHAP =[selectDic[@"chap_enable"] boolValue];
+        modal.MSCHAP2 =[selectDic[@"mschap2_enable"] boolValue];
+        modal.port =selectDic[@"port"];
+        modal.TLS =[selectDic[@"allowInsecure"] boolValue];
+        return modal;
+
+    }
+    return NodeModel.new;
+}
+
+@end
---
+++ vpn_adapter_ios.podspec
@@ -0,0 +1,26 @@
+#
+# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html.
+# Run `pod lib lint sshvpn_flutter.podspec` to validate before publishing.
+#
+Pod::Spec.new do |s|
+  s.name             = 'vpn_adapter_ios'
+  s.version          = '1.0.0'
+  s.summary          = 'Vpn utils for ios native'
+  s.description      = <<-DESC
+Vpn utils for ios native.
+                       DESC
+  s.homepage         = 'http://naviddev.info'
+  s.license          = { :file => './LICENSE' }
+  s.author           = { 'Navid Shokoufeh' => 'Navidshokoufeh.dev@gmail.com.com' }
+  s.source           = { :path => '.' }
+  s.source_files = 'Classes/**/*'
+  s.public_header_files = 'Classes/**/*.h'
+  s.static_framework = true
+  s.platform = :ios, '15.0'
+  s.preserve_path = ['include/ExtParser.framework','include/openconnect.framework','include/SshLib.framework']
+  s.vendored_frameworks  = ['include/ExtParser.framework','include/openconnect.framework','include/SshLib.framework']
+
+  s.vendored_libraries = "**/*.a"
+
+  s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386'}
+end
